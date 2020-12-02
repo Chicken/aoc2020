7f44c59f57e79bdeee4789045718e69ca2ffb2c4
Antti <antti@antti.codes>
2020-12-02 07:54:24 +0200

    day2

---
+++ day2/part2.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const inputFile = fs.readFileSync("./input.txt", "utf-8");
+
+// parse input
+const input = inputFile.split("\n").map(v=>{
+    let [ policy, password ] = v.split(": ");
+    policy = {
+        first: parseInt(policy.split("-")[0]),
+        second: parseInt(policy.split("-")[1].split(" ")[0]),
+        char: policy.split("-")[1].split(" ")[1]
+    }
+    return {
+        policy,
+        password
+    }
+})
+
+let valid = 0;
+
+for(let pw of input) {
+    if(((pw.password[pw.policy.first-1] == pw.policy.char) && (pw.password[pw.policy.second-1] != pw.policy.char))
+    || ((pw.password[pw.policy.first-1] != pw.policy.char) && (pw.password[pw.policy.second-1] == pw.policy.char))) {
+        valid++;
+    }
+}
+
+console.log(valid);
