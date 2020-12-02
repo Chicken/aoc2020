fae991e4ba7deb5bb6e8dcc2c8f8d7390cccd0c7
Antti <antti@antti.codes>
2020-12-02 07:54:24 +0200

    day2

---
+++ day2/part1.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const inputFile = fs.readFileSync("./input.txt", "utf-8");
+
+// parse input
+const input = inputFile.split("\n").map(v=>{
+    let [ policy, password ] = v.split(": ");
+    policy = {
+        min: parseInt(policy.split("-")[0]),
+        max: parseInt(policy.split("-")[1].split(" ")[0]),
+        char: policy.split("-")[1].split(" ")[1]
+    }
+    return {
+        policy,
+        password
+    }
+})
+
+let valid = 0;
+
+for(let pw of input) {
+    let count = (pw.password.match(new RegExp(pw.policy.char, "g")) || []).length
+    if(count <= pw.policy.max && count >= pw.policy.min) valid++;
+}
+
+console.log(valid);
